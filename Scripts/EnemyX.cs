89bd7eed119ef807155d4b5e28cf7bef67e79b72
Yaadieboy <111990104+Yaadieboy@users.noreply.github.com>
2022-10-27 22:20:25 +0200

    Add files via upload

---
+++ Scripts/EnemyX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Rigidbody enemyRb;
+    private GameObject PlayerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        PlayerGoal = GameObject.Find("Player Goal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (PlayerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
